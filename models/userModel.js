c47e3484dbd34d51e31f8284a857dcec344838b0
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-05-21 19:30:43 +0300

    feat(user-model): introduce User schema with validation, timestamps, and update middleware

---
+++ models/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const ROLE = require('../utils/role');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please tell us your name!'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+    role: {
+      type: String,
+      enum: Object.values(ROLE),
+      default: ROLE.TRAINER,
+    },
+  },
+  {
+    // ===> This turns on automatic createdAt & updatedAt
+    timestamps: true,
+
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+userSchema.path('createdAt').select(false);
+userSchema.path('updatedAt').select(false);
+
+userSchema.pre(/^update|.*Update.*$/, function (next) {
+  const update = this.getUpdate();
+  if (this.isNew || !update) return next();
+
+  update.$set = {
+    ...(update.$set || {}),
+    updatedAt: Date.now() - 1000,
+  };
+  this.setUpdate(update);
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
