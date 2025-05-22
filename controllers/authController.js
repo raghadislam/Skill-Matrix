39b814c14770bac1d0fbec64b2cca8e82507a354
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-05-22 22:20:32 +0300

    feat(auth): add signup controller and service with JWT issuance and HTTP-only cookie

---
+++ controllers/authController.js
@@ -0,0 +1,22 @@
+const AuthService = require('../services/authService');
+const sendResponse = require('../utils/sendResponse');
+const catchAsync = require('../utils/catchAsync');
+
+exports.signup = catchAsync(async (req, res) => {
+  const { status, statusCode, token, user } = await AuthService.signup(
+    req.body,
+  );
+
+  const expires = new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+  );
+  const cookieOptions = {
+    expires,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+  };
+  res.cookie('jwt', token, cookieOptions);
+
+  const message = 'Account created successfully.';
+  sendResponse(res, { statusCode, status, message, token, data: { user } });
+});
