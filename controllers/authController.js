5a54f06240eed3333b04aa6b4f2181bd2b9a9078
Raghad Islam <raghad.noser.2003@gmail.com>
2025-05-23 15:28:18 +0300

    feat: add user login functionality with JWT
    - Added  instance method to User model for secure password comparison
    - Implemented login logic in AuthService with JWT generation
    - Integrated login endpoint in AuthController for handling user login requests

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,18 @@ const AuthService = require('../services/authService');
 const sendResponse = require('../utils/sendResponse');
 const catchAsync = require('../utils/catchAsync');
 
+const sendCookie = (res, token) => {
+  const expires = new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+  );
+  const cookieOptions = {
+    expires,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+  };
+  res.cookie('jwt', token, cookieOptions);
+};
+
 exports.signup = catchAsync(async (req, res) => {
   const { status, statusCode, token, user } = await AuthService.signup(
     req.body,
@@ -18,5 +30,12 @@ exports.signup = catchAsync(async (req, res) => {
   res.cookie('jwt', token, cookieOptions);
 
   const message = 'Account created successfully.';
+  sendResponse(res, { statusCode, status, message, token, data: { user } });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { status, statusCode, token, user } = await AuthService.login(req.body);
+  sendCookie(res, token);
+  const message = 'Loggrd in successfully.';
   sendResponse(res, { statusCode, status, message, token, data: { user } });
 });
--- services/authService.js
+++ services/authService.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 
 class AuthService {
   #signToken(id) {
@@ -18,6 +19,27 @@ class AuthService {
     return {
       status: 'success',
       statusCode: 201,
+      token,
+      user,
+    };
+  }
+
+  async login(data) {
+    const { email, password } = data;
+
+    if (!email || !password) {
+      throw new AppError('Please provide email and password', 400);
+    }
+
+    const user = User.findOne({ email });
+    if (!user || !(await user.correctPassword(password))) {
+      throw new AppError('Incorrect email or password', 401);
+    }
+    const token = this.#signToken(user._id);
+
+    return {
+      status: 'success',
+      statusCode: 200,
       token,
       user,
     };
