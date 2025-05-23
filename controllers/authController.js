9d0751e2d9d2e835d534b4e4b49857e045659c98
Raghad Islam <raghad.noser.2003@gmail.com>
2025-05-23 16:43:40 +0300

    refactor: extract sendCookie to responseUtils
    - Created responseUtils.js to centralize response-related utilities
    - Moved sendCookie function to responseUtils
    - Updated signup function to use sendCookie for consistency

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,33 +1,13 @@
 const AuthService = require('../services/authService');
-const sendResponse = require('../utils/sendResponse');
+const { sendResponse, sendCookie } = require('../utils/responseUtils');
 const catchAsync = require('../utils/catchAsync');
-
-const sendCookie = (res, token) => {
-  const expires = new Date(
-    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
-  );
-  const cookieOptions = {
-    expires,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-  };
-  res.cookie('jwt', token, cookieOptions);
-};
 
 exports.signup = catchAsync(async (req, res) => {
   const { status, statusCode, token, user } = await AuthService.signup(
     req.body,
   );
 
-  const expires = new Date(
-    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
-  );
-  const cookieOptions = {
-    expires,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-  };
-  res.cookie('jwt', token, cookieOptions);
+  sendCookie(res, token);
 
   const message = 'Account created successfully.';
   sendResponse(res, { statusCode, status, message, token, data: { user } });
@@ -35,7 +15,9 @@ exports.signup = catchAsync(async (req, res) => {
 
 exports.login = catchAsync(async (req, res, next) => {
   const { status, statusCode, token, user } = await AuthService.login(req.body);
+
   sendCookie(res, token);
+
   const message = 'Logged in successfully.';
   sendResponse(res, { statusCode, status, message, token, data: { user } });
 });
