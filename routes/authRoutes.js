b75610018e71b0e034f4f37300311c4465169a35
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-06-21 14:06:03 +0300

    refactor(auth): change logout endpoint from GET to POST

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,6 +15,6 @@ const router = express.Router();
 router.post('/signup', validate(signupZodSchema), authController.signup);
 router.post('/login', validate(loginZodSchema), authController.login);
 router.post('/refresh', validate(refreshZodSchema), authController.refresh);
-router.get('/logout', validate(logoutZodSchema), authController.logout);
+router.post('/logout', validate(logoutZodSchema), authController.logout);
 
 module.exports = router;
