8a92c01643d0020c5f619cc6e8e096d4747af267
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-06-21 12:08:36 +0300

    feat(auth): validate refresh endpoint with strict Zod schema

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,12 +6,13 @@ const validate = require('../middlewares/validate');
 const {
   signupZodSchema,
   loginZodSchema,
+  refreshZodSchema,
 } = require('../validators/authValidator');
 
 const router = express.Router();
 
 router.post('/signup', validate(signupZodSchema), authController.signup);
 router.post('/login', validate(loginZodSchema), authController.login);
-router.post('/refresh', authController.refresh);
+router.post('/refresh', validate(refreshZodSchema), authController.refresh);
 
 module.exports = router;
